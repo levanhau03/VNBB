4882704f6a6e9a6686a37b9d5479ea04fb65f3b9
levanhau03 <nhatki888@gmail.com>
2024-04-20 18:35:21 +0700

    add orgin_theme

---
+++ theme/orgin_theme/install.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+	Xiuno BBS 4.0 xiuno O
+*/
+
+!defined('DEBUG') AND exit('Forbidden');
+
+
+
+
+
+
+
+
+?>
---
+++ theme/orgin_theme/unstall.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+	Xiuno BBS 4.0 xiuno O
+*/
+
+!defined('DEBUG') AND exit('Forbidden');
+
+
+?>
